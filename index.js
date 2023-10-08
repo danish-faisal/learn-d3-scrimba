b840b73b21cb2e535fa45c7bfb114ff1d0a6e168
Danish Faisal <danishfaisal.wwe@gmail.com>
2023-10-08 19:21:10 +0530

    Data Loading and Binding

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/** Selection and Manipulation */
+/*
 d3.select("h1")
   .style("color", "red")
   .attr("class", "heading")
@@ -8,3 +10,16 @@ d3.select("body").append("p").text("Second Paragraph");
 d3.select("body").append("p").text("Third Paragraph");
 
 d3.selectAll("p").style("color", "blue");
+*/
+
+var dataset = [1, 2, 3, 4, 5];
+
+d3.select("body")
+  .selectAll("p")
+  .data(dataset)
+  .enter()
+  .append("p")
+  //   .text("D3 is awesome!!")
+  .text(function (d) {
+    return d;
+  });
