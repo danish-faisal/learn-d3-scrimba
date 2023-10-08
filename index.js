7c40fc405c8396d010b4818057304e2680daf663
Danish Faisal <danishfaisal.wwe@gmail.com>
2023-10-08 19:40:26 +0530

    Creating a simple bar chart

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+/** Creating a simple bar chart */
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+var svgWidth = 500,
+  svgHeight = 300,
+  barPadding = 5;
+var barWidth = svgWidth / dataset.length;
+
+var svg = d3.select("svg").attr("width", svgWidth).attr("height", svgHeight);
+
+var barChart = svg
+  .selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .attr("y", function (d) {
+    return svgHeight - d;
+  })
+  .attr("height", function (d) {
+    return d;
+  })
+  .attr("width", barWidth - barPadding)
+  .attr("transform", function (d, i) {
+    var translate = [barWidth * i, 0];
+    return "translate(" + translate + ")";
+  });
+
+/** Data Loading and Binding */
+/*
+var dataset = [1, 2, 3, 4, 5];
+
+d3.select("body")
+  .selectAll("p")
+  .data(dataset)
+  .enter()
+  .append("p")
+  //   .text("D3 is awesome!!")
+  .text(function (d) {
+    return d;
+  });
+*/
+
 /** Selection and Manipulation */
 /*
 d3.select("h1")
@@ -11,15 +53,3 @@ d3.select("body").append("p").text("Third Paragraph");
 
 d3.selectAll("p").style("color", "blue");
 */
-
-var dataset = [1, 2, 3, 4, 5];
-
-d3.select("body")
-  .selectAll("p")
-  .data(dataset)
-  .enter()
-  .append("p")
-  //   .text("D3 is awesome!!")
-  .text(function (d) {
-    return d;
-  });
